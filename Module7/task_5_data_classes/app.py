f9f3bb357070834b9337db97338209ec4592b361
Giorgi Mikautadze <131692237+GiorgiMikautadze22@users.noreply.github.com>
2025-01-19 18:29:30 +0400

    feat: added relationships and crud operations

--- Module7/task_5_data_classes/app.py
+++ Module7/task_5_data_classes/app.py
@@ -1,5 +1,4 @@
 import os
-
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, backref, sessionmaker, DeclarativeBase, Mapped, mapped_column
 
@@ -13,12 +12,15 @@ class Product(Base):
     product_number: Mapped[int] = mapped_column(Integer, nullable=False)
     color: Mapped[str] = mapped_column(String, nullable=False)
     # Relationship between category and product
-    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"))
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id", ondelete="CASCADE"))
     category: Mapped["Category"] = relationship("Category",back_populates="products")
 
     # Relationship between product model and product
     product_model_id: Mapped[int] = mapped_column(Integer, ForeignKey("product_models.id"), nullable=True)
     product_model: Mapped["ProductModel"] = relationship("ProductModel",back_populates="product")
+
+    # Relationship between product and order details
+    order_details: Mapped["OrderDetails"] = relationship("OrderDetails", back_populates="product")
 
     sell_start_date: Mapped[str] = mapped_column(String, nullable=False)
     sell_end_date: Mapped[str] = mapped_column(String, nullable=False)
@@ -33,75 +35,95 @@ class Category(Base):
     products: Mapped[list["Product"]] = relationship("Product",back_populates="category")
 
 
-# class Customer(Base):
-#     __tablename__ = "customers"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name_style: Mapped[int] = mapped_column(Integer, nullable=False)
-#     title: Mapped[str] = mapped_column(String, nullable=True)
-#     first_name: Mapped[str] = mapped_column(String, nullable=False)
-#     middle_name: Mapped[str] = mapped_column(String, nullable=False)
-#     last_name: Mapped[str] = mapped_column(String, nullable=False)
-#     suffix: Mapped[str] = mapped_column(String, nullable=True)
-#     company_name: Mapped[str] = mapped_column(String, nullable=True)
-#     sales_person: Mapped[str] = mapped_column(String, nullable=False)
-#     email_address: Mapped[str] = mapped_column(String, nullable=False)
-#     phone: Mapped[str] = mapped_column(String, nullable=False)
-#
-#     # Customer can have many addresses
-#     customer_addresses: Mapped[list["Address"]] = relationship(secondary="customer_addresses")
-#
-#
-# class Address(Base):
-#     __tablename__ = "addresses"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     address_line_1: Mapped[str] = mapped_column(String(250), nullable=False)
-#     address_line_2: Mapped[str] = mapped_column(String(250), nullable=True)
-#     city: Mapped[str] = mapped_column(String, nullable=False)
-#     state: Mapped[str] = mapped_column(String, nullable=False)
-#     postal_code : Mapped[int]= mapped_column(Integer, nullable=False)
-#     country: Mapped[str] = mapped_column(String, nullable=False)
-#
-#     # Relationship between customer and address
-#     customer_address: Mapped[list["Customer"]] = relationship(secondary="customer_addresses",back_populates="last_name")
-#
-# class CustomerAddress(Base):
-#     __tablename__ = "customer_addresses"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     customer_id: Mapped[int] = mapped_column(Integer,ForeignKey("customers.id"), nullable=False, primary_key=True)
-#     address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"), nullable=False, primary_key=True)
-#     address_type: Mapped[int] = mapped_column(String, nullable=False)
-#
-# class Order(Base):
-#     __tablename__ = "orders"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     order_number: Mapped[int] = mapped_column(Integer, nullable=False)
-#     revision_number: Mapped[int] = mapped_column(Integer, nullable=True)
-#     order_date: Mapped[str] = mapped_column(String, nullable=False)
-#     ship_date: Mapped[str] = mapped_column(String, nullable=False)
-#     status: Mapped[int] = mapped_column(Integer, nullable=False)
-#     online_order_flag: Mapped[int] = mapped_column(Integer, nullable=False)
-#     purchase_order_number: Mapped[int] = mapped_column(Integer, nullable=False)
-#     account_number: Mapped[int] = mapped_column(Integer, nullable=False)
-#     customer_id: Mapped[int] = mapped_column(Integer,ForeignKey("customers.id"), nullable=False)
-#     ship_address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"), nullable=False)
-#     bill_address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"), nullable=False)
-#     ship_method: Mapped[str] = mapped_column(String, nullable=False)
-#     credit_card_approval_code: Mapped[int] = mapped_column(Integer, nullable=False)
-#     sub_total: Mapped[int] = mapped_column(Integer, nullable=False)
-#     tax_amount: Mapped[int] = mapped_column(Integer, nullable=False)
-#     freight: Mapped[int] = mapped_column(Integer, nullable=False)
-#     total_due: Mapped[int] = mapped_column(Integer, nullable=False)
-#     comment: Mapped[str] = mapped_column(String, nullable=False)
-#
-# class OrderDetails(Base):
-#     __tablename__ = "order_details"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     order_id: Mapped[int] = mapped_column(Integer, ForeignKey("orders.id"), nullable=False)
-#     order_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
-#     product_id: Mapped[int] = mapped_column(Integer, ForeignKey("products.id"), nullable=False)
-#     unit_price: Mapped[int] = mapped_column(Integer, nullable=False)
-#     unit_price_discount: Mapped[int] = mapped_column(Integer, nullable=False)
-#     line_total: Mapped[int] = mapped_column(Integer, nullable=False)
+class Customer(Base):
+    __tablename__ = "customers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name_style: Mapped[int] = mapped_column(Integer, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    middle_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String, nullable=False)
+    suffix: Mapped[str] = mapped_column(String, nullable=True)
+    company_name: Mapped[str] = mapped_column(String, nullable=True)
+    sales_person: Mapped[str] = mapped_column(String, nullable=False)
+    email_address: Mapped[str] = mapped_column(String, nullable=False)
+    phone: Mapped[str] = mapped_column(String, nullable=False)
+
+    # Customer can have many addresses
+    addresses: Mapped[list["Address"]] = relationship("Address",back_populates="customer_address")
+
+    # Customer can have many orders
+    orders: Mapped[list["Order"]] = relationship("Order",back_populates="customer_order")
+
+class Address(Base):
+    __tablename__ = "addresses"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    address_line_1: Mapped[str] = mapped_column(String(250), nullable=False)
+    address_line_2: Mapped[str] = mapped_column(String(250), nullable=True)
+    city: Mapped[str] = mapped_column(String, nullable=False)
+    state: Mapped[str] = mapped_column(String, nullable=False)
+    postal_code : Mapped[int]= mapped_column(Integer, nullable=False)
+    country: Mapped[str] = mapped_column(String, nullable=False)
+
+    # Relationship between customer and address
+    customer_id: Mapped[int] = mapped_column(Integer, ForeignKey("customers.id", ondelete="CASCADE"))
+    customer_address: Mapped[list["Customer"]] = relationship("Customer",back_populates="addresses")
+
+    # Relationship between order and address
+    order_billing: Mapped[list["Order"]] = relationship("Order",back_populates="bill_address")
+    order_shipping: Mapped[list["Order"]] = relationship("Order",back_populates="ship_address")
+
+class Order(Base):
+    __tablename__ = "orders"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_number: Mapped[int] = mapped_column(Integer, nullable=False)
+    revision_number: Mapped[int] = mapped_column(Integer, nullable=True)
+    order_date: Mapped[str] = mapped_column(String, nullable=False)
+    ship_date: Mapped[str] = mapped_column(String, nullable=False)
+    status: Mapped[int] = mapped_column(Integer, nullable=False)
+    online_order_flag: Mapped[int] = mapped_column(Integer, nullable=False)
+    purchase_order_number: Mapped[int] = mapped_column(Integer, nullable=False)
+    account_number: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    # Relationship between customer and order
+    customer_id: Mapped[int] = mapped_column(Integer,ForeignKey("customers.id"), nullable=False)
+    customer_order: Mapped[list["Customer"]] = relationship("Customer",back_populates="orders")
+
+    # Relationship between customer address and order
+    ship_address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"), nullable=False)
+    ship_address: Mapped["Address"] = relationship("Address", back_populates="order_shipping")
+
+    bill_address_id: Mapped[int] = mapped_column(Integer, ForeignKey("addresses.id"), nullable=False)
+    bill_address: Mapped["Address"] = relationship("Address", back_populates="order_billing")
+
+    # Relationship between order and order details
+    order_details: Mapped["OrderDetails"] = relationship("OrderDetails", back_populates="order")
+
+    ship_method: Mapped[str] = mapped_column(String, nullable=False)
+    credit_card_approval_code: Mapped[int] = mapped_column(Integer, nullable=False)
+    sub_total: Mapped[int] = mapped_column(Integer, nullable=False)
+    tax_amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    freight: Mapped[int] = mapped_column(Integer, nullable=False)
+    total_due: Mapped[int] = mapped_column(Integer, nullable=False)
+    comment: Mapped[str] = mapped_column(String, nullable=False)
+
+class OrderDetails(Base):
+    __tablename__ = "order_details"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Relationship between order and order details
+    order_id: Mapped[int] = mapped_column(Integer, ForeignKey("orders.id"), nullable=False)
+    order: Mapped["Order"] = relationship("Order", back_populates="order_details")
+
+    order_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    # Relationship between order detail and product
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey("products.id"), nullable=False)
+    product: Mapped["Product"] = relationship("Product", back_populates="order_details")
+
+    unit_price: Mapped[int] = mapped_column(Integer, nullable=False)
+    unit_price_discount: Mapped[int] = mapped_column(Integer, nullable=False)
+    line_total: Mapped[int] = mapped_column(Integer, nullable=False)
 
 class ProductModel(Base):
     __tablename__ = "product_models"
@@ -111,6 +133,7 @@ class ProductModel(Base):
     product: Mapped[list["Product"]] = relationship("Product", back_populates="product_model")
 
 
+# Initialize Database
 base_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(base_dir, "final-project.db")
 database_url = f"sqlite:///{db_path}"
--- Module7/task_7/app.py
+++ Module7/task_7/app.py
@@ -1,5 +1,5 @@
-from Module7.task_5_data_classes.app import SessionLocal, Category, Product, ProductModel
-from sqlalchemy import select
+from Module7.task_5_data_classes.app import SessionLocal, Category, Product, ProductModel, Customer, Address, Order, OrderDetails
+from sqlalchemy import select, update, delete
 
 
 class CategoryRepository:
@@ -22,9 +22,30 @@ class CategoryRepository:
 
     def get_all_categories(self):
         session = SessionLocal()
-        categories = session.execute(select(Category)).all().scalars()
+        categories = session.execute(select(Category)).scalars().all()
         session.close()
         return categories
+
+    def update_category(self, category_id, name=None, parent_id=None):
+        session = SessionLocal()
+
+        category = session.execute(select(Category).where(Category.id == category_id)).scalar_one_or_none()
+        if name:
+            category.name = name
+        if parent_id:
+            category.parent_category_id = parent_id
+
+        session.commit()
+        session.close()
+        print("Category has been updated")
+
+    def delete_category(self, category_id):
+        session = SessionLocal()
+        category = session.execute(select(Category).where(Category.id == category_id)).scalar_one_or_none()
+        session.delete(category)
+        session.commit()
+        session.close()
+        print("Category has been deleted")
 
 
 class ProductModelRepository:
@@ -38,6 +59,26 @@ class ProductModelRepository:
         session.commit()
         session.close()
         print("New Model has been created")
+
+    def get_model(self, id):
+        session = SessionLocal()
+        model = session.execute(select(ProductModel).where(ProductModel.id == id)).scalar_one_or_none()
+        session.close()
+        return model
+
+    def get_all_categories(self):
+        session = SessionLocal()
+        categories = session.execute(select(ProductModel)).scalars().all()
+        session.close()
+        return categories
+
+    def delete_model(self, model_id):
+        session = SessionLocal()
+        model = session.execute(select(ProductModel).where(ProductModel.id == model_id)).scalar_one_or_none()
+        session.delete(model)
+        session.commit()
+        session.close()
+        print("Category has been deleted")
 
 class ProductRepository:
     def create_product(self, name, product_number, color, category, start, end, discount_date, product_model = None):
@@ -63,12 +104,269 @@ class ProductRepository:
         session.close()
         print("New Product has been created")
 
+    def get_product_by_id(self, product_id: int):
+        session = SessionLocal()
+        return session.get(Product, product_id)
 
-electronics = CategoryRepository()
-# electronics.create_category("Electronics")
-#
-# model = ProductModelRepository()
-# model.create_model("Iphone", "Apple Phones")
-#
-# product = ProductRepository()
-# product.create_product("Iphone", 22, "red", "Electronics", "22 august", "30 august", "25 august")
+    def get_all_products(self):
+        session = SessionLocal()
+        return session.execute(select(Product)).scalars().all()
+
+    def update_product(self, product_id: int, **kwargs):
+        session = SessionLocal()
+        session.execute(
+            update(Product)
+            .where(Product.id == product_id)
+            .values(**kwargs)
+        )
+        session.commit()
+
+    def delete_product(self, product_id: int):
+        session = SessionLocal()
+        session.execute(
+            delete(Product)
+            .where(Product.id == product_id)
+        )
+        session.commit()
+
+class CustomerRepository:
+    def create_customer(self, name_style: int, first_name: str, middle_name: str, last_name: str, sales_person: str, email_address: str, phone: str, title: str = None, suffix: str = None,company_name: str = None):
+
+        session = SessionLocal()
+        new_customer = Customer(
+            name_style=name_style,
+            title=title,
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name,
+            suffix=suffix,
+            company_name=company_name,
+            sales_person=sales_person,
+            email_address=email_address,
+            phone=phone
+        )
+        session.add(new_customer)
+        session.commit()
+        session.refresh(new_customer)
+        return new_customer
+
+    def get_customer_by_id(self, customer_id: int):
+        session = SessionLocal()
+        return session.get(Customer, customer_id)
+
+    def get_all_customers(self):
+        session = SessionLocal()
+        return session.execute(select(Customer)).scalars().all()
+
+    def update_customer(self, customer_id: int, **kwargs):
+        session = SessionLocal()
+        session.execute(
+            update(Customer)
+            .where(Customer.id == customer_id)
+            .values(**kwargs)
+        )
+        session.commit()
+
+    def delete_customer(self, customer_id: int):
+        session = SessionLocal()
+        session.execute(
+            delete(Customer)
+            .where(Customer.id == customer_id)
+        )
+        session.commit()
+
+class AddressRepository:
+
+    def create_address(self, address_line_1: str, city: str, state: str, postal_code: int, country: str,
+                       customer_id: int, address_line_2: str = None):
+        session = SessionLocal()
+        new_address = Address(
+            address_line_1=address_line_1,
+            address_line_2=address_line_2,
+            city=city,
+            state=state,
+            postal_code=postal_code,
+            country=country,
+            customer_id=customer_id
+        )
+        session.add(new_address)
+        session.commit()
+        session.refresh(new_address)
+        return new_address
+
+    def get_address_by_id(self, address_id: int):
+        session = SessionLocal()
+
+        return session.get(Address, address_id)
+
+    def get_all_addresses(self):
+        session = SessionLocal()
+
+        return session.execute(select(Address)).scalars().all()
+
+    def get_addresses_by_customer_id(self, customer_id: int):
+        session = SessionLocal()
+
+        return session.execute(
+            select(Address).where(Address.customer_id == customer_id)
+        ).scalars().all()
+
+    def update_address(self, address_id: int, **kwargs):
+        session = SessionLocal()
+
+        session.execute(
+            update(Address)
+            .where(Address.id == address_id)
+            .values(**kwargs)
+        )
+        session.commit()
+
+    def delete_address(self, address_id: int):
+        session = SessionLocal()
+
+        session.execute(
+            delete(Address)
+            .where(Address.id == address_id)
+        )
+        session.commit()
+
+class OrderRepository:
+    def create_order(
+            self,
+            order_number: int,
+            order_date: str,
+            ship_date: str,
+            status: int,
+            online_order_flag: int,
+            purchase_order_number: int,
+            account_number: int,
+            customer_id: int,
+            ship_address_id: int,
+            bill_address_id: int,
+            ship_method: str,
+            credit_card_approval_code: int,
+            sub_total: int,
+            tax_amount: int,
+            freight: int,
+            total_due: int,
+            comment: str,
+            revision_number: int = None
+    ):
+        session = SessionLocal()
+
+        new_order = Order(
+            order_number=order_number,
+            revision_number=revision_number,
+            order_date=order_date,
+            ship_date=ship_date,
+            status=status,
+            online_order_flag=online_order_flag,
+            purchase_order_number=purchase_order_number,
+            account_number=account_number,
+            customer_id=customer_id,
+            ship_address_id=ship_address_id,
+            bill_address_id=bill_address_id,
+            ship_method=ship_method,
+            credit_card_approval_code=credit_card_approval_code,
+            sub_total=sub_total,
+            tax_amount=tax_amount,
+            freight=freight,
+            total_due=total_due,
+            comment=comment,
+        )
+        session.add(new_order)
+        session.commit()
+        session.refresh(new_order)
+        return new_order
+
+    def get_order_by_id(self, order_id: int):
+        session = SessionLocal()
+
+        return session.get(Order, order_id)
+
+    def get_all_orders(self):
+        session = SessionLocal()
+
+        return session.execute(select(Order)).scalars().all()
+
+    def get_orders_by_customer_id(self, customer_id: int):
+        session = SessionLocal()
+
+        return session.execute(
+            select(Order).where(Order.customer_id == customer_id)
+        ).scalars().all()
+
+    def update_order(self, order_id: int, **kwargs):
+        session = SessionLocal()
+
+        session.execute(
+            update(Order)
+            .where(Order.id == order_id)
+            .values(**kwargs)
+        )
+        session.commit()
+
+    def delete_order(self, order_id: int):
+        session = SessionLocal()
+
+        session.execute(
+            delete(Order)
+            .where(Order.id == order_id)
+        )
+        session.commit()
+
+class OrderDetailsRepository:
+    def create_order_detail(
+            self,
+            order_id: int,
+            order_quantity: int,
+            product_id: int,
+            unit_price: int,
+            unit_price_discount: int,
+            line_total: int
+    ):
+        session = SessionLocal()
+
+        new_order_detail = OrderDetails(
+            order_id=order_id,
+            order_quantity=order_quantity,
+            product_id=product_id,
+            unit_price=unit_price,
+            unit_price_discount=unit_price_discount,
+            line_total=line_total,
+        )
+        session.add(new_order_detail)
+        session.commit()
+        session.refresh(new_order_detail)
+        return new_order_detail
+
+    def get_order_detail_by_id(self, order_detail_id: int):
+        session = SessionLocal()
+
+        return session.get(OrderDetails, order_detail_id)
+
+    def get_order_details_by_order_id(self, order_id: int):
+        session = SessionLocal()
+
+        return session.execute(
+            select(OrderDetails).where(OrderDetails.order_id == order_id)
+        ).scalars().all()
+
+    def update_order_detail(self, order_detail_id: int, **kwargs):
+        session = SessionLocal()
+
+        session.execute(
+            update(OrderDetails)
+            .where(OrderDetails.id == order_detail_id)
+            .values(**kwargs)
+        )
+        session.commit()
+
+    def delete_order_detail(self, order_detail_id: int):
+        session = SessionLocal()
+
+        session.execute(
+            delete(OrderDetails)
+            .where(OrderDetails.id == order_detail_id)
+        )
+        session.commit()
